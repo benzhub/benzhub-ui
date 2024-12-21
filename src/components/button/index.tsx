e20379807aaa6c389af8dadd85670186ca78e353
Len Rich <lenrich1403@gmail.com>
2024-12-21 15:30:47 +0800

    style: Add size and border radius props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,14 +1,29 @@
 import classNames from "classnames";
 
-function Button({ title, variant = "contained", onClick, disabled }: ButtonProps) {
+function Button({
+  title,
+  variant = "contained",
+  onClick,
+  disabled,
+  size = "medium",
+  rounded = "none",
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={classNames("py-2 px-5 cursor-pointer", {
+      className={classNames("cursor-pointer shadow-none text-sm px-5", {
         "border border-solid border-black bg-white text-black":
           variant === "outlined",
         "bg-black text-white": variant === "contained",
-        "pointer-events-none bg-gray-300 border-none cursor-not-allowed": disabled
+        "pointer-events-none bg-gray-300 border-none cursor-not-allowed":
+          disabled,
+        "h-8": size === "small",
+        "h-10": size === "medium",
+        "h-12": size === "large",
+        "rounded-none": rounded === "none",
+        "rounded-sm": rounded === "small",
+        "rounded-md": rounded === "medium",
+        "rounded-lg": rounded === "large",
       })}
     >
       {title}
@@ -23,4 +38,6 @@ export interface ButtonProps {
   variant?: "outlined" | "contained";
   onClick?: () => void;
   disabled?: boolean;
+  size?: "small" | "medium" | "large";
+  rounded?: "none" | "small" | "medium" | "large";
 }
