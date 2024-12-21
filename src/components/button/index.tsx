1041b2fb96ad18468aa65f58f09640190b61c8f4
Len Rich <lenrich1403@gmail.com>
2024-12-21 12:10:03 +0800

    feat: setup first component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+// import React from 'react'
 
-function index() {
-  return (
-    <div>index</div>
-  )
-}
-
-export default index
+function Button({ title, variant = "contained" }: ButtonProps) {
+    let styles: React.CSSProperties = {
+      padding: "5px 10px",
+    };
+  
+    if (variant === "outline") {
+      styles = {
+        ...styles,
+        backgroundColor: "transparent",
+        border: "1px solid black",
+      };
+    }
+  
+    if (variant === "contained") {
+      styles = {
+        ...styles,
+        backgroundColor: "black",
+        color: "white",
+      };
+    }
+    return <button style={styles}>{title}</button>;
+  }
+  
+  export default Button;
+  
+  export interface ButtonProps {
+    title: string;
+    variant?: "outline" | "contained";
+  }
+  
