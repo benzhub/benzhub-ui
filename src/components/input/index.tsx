58727daca21d820b89d7bed3c3b9cf6ecc6d2770
Len Rich <lenrich1403@gmail.com>
2025-01-07 15:46:02 +0800

    chore: build npm to publish

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -34,18 +34,22 @@ function Input({
           disabled={disabled}
           className={classNames(
             "px-5",
-            "focus:border-2 focus:border-primary focus:shadow-[0_0_0_1px_var(--primary-color)]", {
-            "h-8": size === "small",
-            "h-10": size === "medium",
-            "h-12": size === "large",
-            "rounded-sm": rounded === "small",
-            "rounded-md": rounded === "medium",
-            "rounded-lg": rounded === "large",
-            "rounded-full": rounded === "full",
-            "border border-primary border-solid outline-none transition-all duration-": !disabled,
-            "border border-red-500 border-solid": error,
-            "cursor-not-allowed pointer-events-none bg-gray-100 border border-solid": disabled,
-          })}
+            "focus:border-2 focus:border-primary focus:shadow-[0_0_0_1px_var(--primary-color)]",
+            {
+              "h-8": size === "small",
+              "h-10": size === "medium",
+              "h-12": size === "large",
+              "rounded-sm": rounded === "small",
+              "rounded-md": rounded === "medium",
+              "rounded-lg": rounded === "large",
+              "rounded-full": rounded === "full",
+              "border border-primary border-solid outline-none transition-all duration-":
+                !disabled,
+              "border border-red-500 border-solid": error,
+              "cursor-not-allowed pointer-events-none bg-gray-100 border border-solid":
+                disabled,
+            }
+          )}
         />
         {error && <span className="text-red-500">{error}</span>}
       </div>
