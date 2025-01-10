0f618c4f339c0d6fb7e6a1ce6303076dee46f32d
Len Rich <lenrich1403@gmail.com>
2025-01-10 14:37:19 +0800

    chore: build storybook

--- storybook-static/assets/theme-provider.stories-d2Lrb8Bm.js
+++
@@ -1,7 +0,0 @@
-import{T as a}from"./index-CYu3-ncJ.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const c={title:"ThemeProvider",component:a,tags:["autodocs"]},r={args:{primaryColor:"#000"}};var e,o,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    primaryColor: "#000"
-  }
-} as {
-  args: ThemeProviderProps;
-}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const n=["BasicTheme"];export{r as BasicTheme,n as __namedExportsOrder,c as default};
